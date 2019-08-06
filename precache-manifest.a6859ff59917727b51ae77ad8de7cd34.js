ed2fc35e243920b1ca66dc01e6d395cdd58fc2ec
Fahran Deen <fahrandeen17@gmail.com>
2019-08-06 13:44:41 +0530

    Updates

---
+++ precache-manifest.a6859ff59917727b51ae77ad8de7cd34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f19e3dfeaa8c06a06da1bd1600ab17",
+    "url": "/myapp0107/index.html"
+  },
+  {
+    "revision": "690dd0cba12f172b0fb2",
+    "url": "/myapp0107/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9550b97a394aa9471cda",
+    "url": "/myapp0107/static/css/main.45f6a613.chunk.css"
+  },
+  {
+    "revision": "690dd0cba12f172b0fb2",
+    "url": "/myapp0107/static/js/2.bdfef20c.chunk.js"
+  },
+  {
+    "revision": "9550b97a394aa9471cda",
+    "url": "/myapp0107/static/js/main.3d3ef123.chunk.js"
+  },
+  {
+    "revision": "c5efa4c605be61914a6f",
+    "url": "/myapp0107/static/js/runtime~main.9e04e806.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp0107/static/media/logo.5d5d9eef.svg"
+  }
+]);
